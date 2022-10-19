579597c1adc32ecef00d07ff3df9eab08236114a
adammkeane <adamkeane.edu@gmail.com>
2022-10-19 20:16:58 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,19 +10,30 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }  
         })
     }
+
+    runGame("addition");
 })
 
 /**
  * The main game 'loop', called when the script is first loaded
  * and afer the user's answer has processed.
  */
-function runGame() {
+function runGame(gameType) {
+
+    //create two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) +1;
     let num2 = Math.floor(Math.random() * 25) +1;
+
+    if (gameType === "addition") {
+        displayAdditionQuesion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`
+    }
 }
 
 function checkAnswer() {
@@ -41,8 +52,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuesion() {
-
+function displayAdditionQuesion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
 }
 
 function displaySubtractQuesion() {
