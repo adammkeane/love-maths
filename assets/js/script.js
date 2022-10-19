78e6dad66f3988de9ec84fd77a0f67ebb781db79
adammkeane <adamkeane.edu@gmail.com>
2022-10-19 22:35:44 +0000

    Add division game

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,9 +43,13 @@ function runGame(gameType) {
     } else  if (gameType === "subtract") {
         if (num2>num1) {
             displaySubtractQuestion(num2, num1);
-        } else
+        } else {
         displaySubtractQuestion(num1, num2);
-    }else {
+        }
+    } else  if (gameType === "division") {
+        let num3 = num1*num2;
+        displayDivisionQuestion(num3, num2);
+    } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`
     }
@@ -89,7 +93,9 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else  if (operator === "-") {
         return [operand1 - operand2, "subtract"];
-    }else {
+    } else  if (operator === "/") {
+        return [operand1 / operand2, "division"];
+    } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`
     } 
@@ -128,5 +134,11 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById("operand1").textContent = operand1;
     document.getElementById("operand2").textContent = operand2;
     document.getElementById("operator").textContent = "x";
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "/";
 }
 
